980978e5d58d4cbba274e5a1a18003332570d0b4
unknown <jaisharmafps@gmail.com>
2020-11-03 00:25:59 +0530

    v_2020.0.2

--- GFH.py
+++ GFH.py
@@ -1,23 +1,32 @@
+#!/usr/bin/python3
 import sys
 import requests
-import argparse
-import colorama
+import os.path
 
-from re import search
-from termcolor import colored
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from frontend.strings import art1, art2, email, splitter
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-colorama.init()
-print (colored(art1, 'green'), colored(art2+email+splitter, 'cyan'))
+print (art1+art2+email+splitter)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-u', help='target endpoint{url/api}', dest='url')
+#User input
+file_path=sys.argv[1]
 
-args= parser.parse_args()
-target = args.url
-print(colored("Scanning Web-Headers for: ", 'green')+target)
-
-r = requests.get(target)
-
-headRaw = r.headers
-print(headRaw)
+#Function to run GET requests
+def req(url):
+    print(url)
+    b = requests.get(url, verify=False)
+    print("\nScanned: "+str(b.url))
+    print("\nStatus Code: "+str(b.status_code))
+    head = str(b.headers)
+    if "Content-Security-Policy" not in head:
+        print("\n Missing CSP Header")
+    if "X-XSS-Protection" not in head:
+        print("\n Missing X-XSS Header")
+    if "Strict-Transport-Security" not in head:
+        print("\n Missing HSTS Header")
+    if "X-Frame-Options" not in head:
+        print("\n Missing X-Frame Header")
+    if "X-Content-Type-Options" not in head:
+        print("\n Missing X-Content-Type Header")
+req(file_path)
--- frontend/strings.py
+++ frontend/strings.py
@@ -4,8 +4,8 @@ art1 = r"""
   _____       ___           __ __            __
  / ___/__    / _/__  ____  / // /__ ___ ____/ /
 / (_ / _ \  / _/ _ \/ __/ / _  / -_) _ `/ _  /
-\___/\___/ /_/ \___/_/   /_//_/\__/\_,_/\_,_/ ~v2019.0.1 | jai1sharma
+\___/\___/ /_/ \___/_/   /_//_/\__/\_,_/\_,_/ ~v2020.0.2 | ja1sharma
     """
-art2 = "\n\nTest how good your Security Headers are!"
-email = "\nFor queries: jaisharmafps2@gmail.com\n"
-splitter = "---\n"
+art2 = "\n\nQuick Security Headers Check!"
+email = "\njaisharmafps2@gmail.com\n"
+splitter = "\n++++++++++++++++++++++++++++++++++++++++\n"
